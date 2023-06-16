32825a15e4853565767bab7918c52edb02066f26
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-06-16 22:12:29 +0300

    modal gallery

--- js/galleryOverlay.js
+++ js/galleryOverlay.js
@@ -4,31 +4,47 @@ console.dir(backdrop)
 
 gallery.addEventListener('click', renderModal)
 const button = document.querySelector('.btn__overlay')
-console.log(button)
+const modalList = document.querySelector('.modal__list');
 
 function renderModal(e) {
     e.preventDefault();
     if (e.target.nodeName !== 'IMG' && e.target.nodeName !== 'PRE' && e.target.nodeName !== 'H3' && e.target.nodeName !== 'BUTTON') {
         return;
     }
-
     if (e.target.nodeName === 'BUTTON') {
         button.addEventListener('click', toggleModal())
-        // const pulse_hand = document.querySelectorAll('#pulse-icon');
-        // console.log(pulse_hand);
-        // pulse_hand.style.display = 'none'; 
         if (e.target.id === '3v1') {
-        console.log('dadadaa')
+            const modalBody = `
+        <li class= modal__item><img width = 100% src='../img/3v12.jpeg' alt="swing"></li>
+        <li class= modal__item><img width = 100% src='../img/3v13.jpeg' alt="swing"></li>
+        <li class= modal__item><img width = 100% src='../img/3v14.jpeg' alt="swing"></li>
+            `;
+            modalList.insertAdjacentHTML('beforeend', modalBody) 
         }        
         if (e.target.id === 'bunnySwing') {
-            console.log('trrrr')
+            const modalBody = `
+        <li class= modal__item><img width= 100% src='../img/bunnySwing2.jpeg' alt="swing"></li>
+        <li class= modal__item><img width= 100% src='../img/bunnySwing.jpeg' alt="swing"></li>
+        <li class= modal__item><img width= 100% src='../img/bunnySwing4.jpeg' alt="swing"></li>
+            `;
+            modalList.insertAdjacentHTML('beforeend', modalBody) 
+
         }
     }
 }
 
 const modalClose = document.querySelector("[data-modal-close]")
-modalClose.addEventListener('click', toggleModal)
+modalClose.addEventListener('click', () => {
+    toggleModal()
+    modalList.innerHTML = '';
+})
+
 
 function toggleModal() { 
     backdrop.classList.toggle('is-hidden')
 }
+
+
+
+
+
