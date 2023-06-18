27f40b18f91206a647f13b37f827d1d9ac6fa550
Sakhatskyi Mark <110189603+Markizdickabraz@users.noreply.github.com>
2023-06-18 15:24:14 +0300

    done?

--- js/openTelegram.js
+++ js/openTelegram.js
@@ -1,26 +1,27 @@
 const telegramLink = document.getElementById('telegram-link');
+
+telegramLink.addEventListener('click', function(event) {
+  event.preventDefault();
   
-  telegramLink.addEventListener('click', function(event) {
-    event.preventDefault();
-    
-    if (isTelegramAppInstalled()) {
-      openTelegramApp();
-    } else {
-    //   openTelegramWeb();
-    }
-  });
-  
-  function isTelegramAppInstalled() {
-    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
-    
-    return userAgent.indexOf('Telegram') !== -1;
+  if (isTelegramAppInstalled()) {
+    openTelegramApp();
+  } else {
+    openTelegramWeb();
   }
+});
+
+function isTelegramAppInstalled() {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
   
-  function openTelegramApp() {
-    window.location.href = 'tg://k/#@nastya_17_08';
-  }
-  
-//   function openTelegramWeb() {
-//     window.location.href = 'https://web.telegram.org/k/#@nastya_17_08';
-//   }
+  return userAgent.indexOf('Telegram') !== -1;
+}
+
+function openTelegramApp() {
+  window.location.href = 'tg://resolve?domain=nastya_17_08';
+}
+
+function openTelegramWeb() {
+  window.location.href = 'https://web.telegram.org/k/#@nastya_17_08';
+}
+
 
